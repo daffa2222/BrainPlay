0d8fd84767400d36f10fe610a814b0399219a44c
daffa2222 <daffausman08@gmail.com>
2026-06-07 01:19:36 +0800

    chore(config): menambah library retrofit dan izin internet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // TARO DI SINI DAFFA (DI BAWAH ESPRESSO CORE)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
