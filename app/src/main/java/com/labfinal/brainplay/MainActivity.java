413084dc4210de917206ce0f71c2f1945596b19f
daffa2222 <daffausman08@gmail.com>
2026-06-06 23:31:22 +0800

    fix(logic): memperbaiki error findViewByID dan merapikan MainActivity

--- app/src/main/java/com/labfinal/brainplay/MainActivity.java
+++ app/src/main/java/com/labfinal/brainplay/MainActivity.java
@@ -1,24 +1,14 @@
 package com.labfinal.brainplay;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // Memanggil layout activity_main.xml yang sudah kita desain cokelat cream
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
